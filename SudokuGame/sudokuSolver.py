d5f40cfb7a8bb10888f10a1a481b829f8c08a317
enes <gulcenes1@gmail.com>
2021-01-31 12:13:57 +0300

    Up.

---
+++ SudokuGame/sudokuSolver.py
@@ -0,0 +1,63 @@
+import pyautogui as pg
+import time
+import numpy as np
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+
+def possible(board, y, x, val):  # y row , x cols
+    if val in board[y]:  # Check rows
+        return False
+    for v in range(9):  # Check cols
+        if board[v][x] == val:
+            return False
+
+    sqy = (y // 3) * 3  # y//3 0,1,2 *3 to find index
+    sqx = (x // 3) * 3  # x//3 0,1,2
+
+    for r in range(sqy, sqy + 3):  # Check Squares
+        for c in range(sqx, sqx + 3):
+            if board[r][c] == val:
+                return False
+    return True
+
+
+def find_empty(board):
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == 0:
+                return r, c  # row, col
+
+    return None
+
+
+def solve(board):
+    empty_cell = find_empty(board)
+
+    if not empty_cell:
+        return True
+    else:
+        row, col = empty_cell
+
+    for i in range(1, 10):
+        if possible(board, row, col, i):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+solve(board)
+print(np.matrix(board))
